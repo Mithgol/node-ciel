cb494c8d7963948d8a4ff7f1a9f52104d8cbbc8e
Mithgol the Webmaster <getgit@mithgol.ru>
2015-10-09 21:26:01 +0300

    initial version of the intended source code

--- ciel.js
+++ ciel.js
@@ -1,1 +1,22 @@
-module.exports = 'Hello world.';
+module.exports = {
+   ok: function(logElement){
+      ansi.black().bg.green().write('  OK  ').reset().write(' ');
+      console.log(logElement);
+   },
+   fail: function(logElement){
+      ansi.brightWhite().bg.red().write(' FAIL ').reset().write(' ');
+      console.log(logElement);
+   },
+   status: function(logElement){
+      ansi.brightYellow().bg.blue().write('STATUS').reset().write(' ');
+      console.log(logElement);
+   },
+   dupe: function(logElement){
+      ansi.brightCyan().bg.magenta().write(' DUPE ').reset().write(' ');
+      console.log(logElement);
+   },
+   skip: function(logElement){
+      ansi.brightGreen().bg.blue().write(' SKIP ').reset().write(' ');
+      console.log(logElement);
+   }
+};
